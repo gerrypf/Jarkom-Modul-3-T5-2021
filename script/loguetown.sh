ba61f20ef181227e4d8db8635cf50e7bfa2eff80
gerrypf <73151831+gerrypf@users.noreply.github.com>
2021-11-13 14:31:21 +0700

    Create loguetown.sh

---
+++ script/loguetown.sh
@@ -0,0 +1,6 @@
+apt-get update
+apt-get install speedtest-cli -y
+apt-get install ca-certificates openssl -y
+apt-get install dnsutils -y
+apt-get install lynx -y
+export http_proxy="http://jualbelikapal.t05.com:5000"
